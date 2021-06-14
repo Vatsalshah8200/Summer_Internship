e2a21bbd3cdebaadee15f39f0cfd909f397157a2
Vatsalshah8200 <202012042@daiict.ac.in>
2021-06-14 11:46:57 +0530

    issue with q3

--- winter2019/q3back.js
+++ winter2019/q3back.js
@@ -1,24 +1,31 @@
-var http = require("http");
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
 var fs = require("fs");
-var util = require('util')
+var util = require('util');
+
 
 var stock = {
 "Apple":15,
 "Orange":20,
 "Grapes":15
 };
-http.createServer(function(req,res){
+const app= express();
+var server = app.listen(8080,()=>{
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('running at http://localhost:' + port);
+}) 
+// app.set('view engine','html')
+app.use(bodyParser.urlencoded({extended:false}));
 
-
-    console.log("server started");
-
-    
-    console.log('Request received: ');
-    req.on('data', function (chunk) {
-        console.log('GOT DATA!');
-    });
-    
-    return res.end('callback(\'{\"msg\": \"OK\"}\')');
-    
-  
-}).listen(8080);
+ app.use(express.static("assets"));
+app.get("/",function(req,res){
+    // app.use(express.static(__dirname + '/q3front'));
+     res.sendFile(path.join(__dirname,"/q3front.html"));
+     
+});
+app.post("/ajaxdemo",function(req,res){
+    var data = req.body;
+    console.log(data);
+});
